3a5be6a18b8f3ec1dcdf97b487518efbcc978a6e
Zlatan Radovanovic <zlatan.radovanovic@fet.ba>
2021-03-29 23:33:10 +0200

    Mostly refactoring

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"os"
 	"strings"
 	"time"
@@ -37,40 +38,14 @@ func main() {
 		action, args := readInput()
 		switch action {
 		case "Dial":
-			if len(args) < 2 {
-				log.Error("at least two endpoints required")
-			} else {
-				bridge := createBridge(cl)
-
-				for _, ch := range args {
-					call(cl, bridge, ch)
-				}
-				if len(args) == 2 {
-					//call between two endpoints
-				} else {
-					//conference
-				}
-			}
+			Dialer(cl, args)
 		case "JoinCall":
-			//todo - first arg will be bridgeid of bridge to be added to, second id is endpoint
-			channel := createChannel(cl, args[1])
-			if channel == nil {
-				return
+			if err := JoinBridge(cl, args[0], args[1]); err != nil {
+				log.Error("failed to join bridge", "error", err)
 			}
-			bridge := bridges[args[0]]
-			if bridge == nil {
-				log.Error("this bridge does not exist")
-			} else {
-				bridge.AddChannel(channel.ID())
-			}
+			//TODO: case when we Join 2-person call, make sure that Dialer knows it "became" a conference
 		case "ListCalls":
-			for _, bridge := range bridges {
-				bridgeData, _ := bridge.Data()
-				log.Info("Call data: ")
-				for _, channelID := range bridgeData.ChannelIDs {
-					log.Info("Channel ID: ", "channelID", channels[channelID])
-				}
-			}
+			ListCalls()
 		default:
 			log.Error("bad choice")
 		}
@@ -78,55 +53,128 @@ func main() {
 
 }
 
-func call(cl ari.Client, bridge *ari.BridgeHandle, ch string) {
-	channel := createChannel(cl, ch)
-	if channel == nil {
+func ListCalls() {
+	if len(bridges) == 0 {
+		log.Warn("No active calls")
 		return
 	}
 
-	if err := channel.Dial("ARI", 15*time.Second); err != nil {
-		log.Error("call failed", "error", err)
+	for _, bridge := range bridges {
+		bridgeData, _ := bridge.Data()
+		log.Info("CALL DATA", "bridgeID", bridgeData.ID)
+		for _, channelID := range bridgeData.ChannelIDs {
+			//I like nested for loops what you gonna do?
+			log.Info("Channel ID: ", "channelID", channels[channelID])
+		}
+	}
+}
+
+func Dialer(cl ari.Client, args []string) {
+	if len(args) < 2 {
+		log.Warn("at least two endpoints required")
+	} else {
+		bridge := createBridge(cl)
+		if bridge == nil {
+			log.Error("failed to create bridge")
+			return
+		}
+
+		if err := addEndpoints(cl, bridge, args); err != nil {
+			log.Error("failed to add endpoints", "error", err)
+		}
+
+		if len(args) == 2 {
+			go manageCall(cl, bridge)
+		}
+	}
+}
+
+func manageCall(cl ari.Client, bridge *ari.BridgeHandle) {
+	sub := cl.Bus().Subscribe(nil, "StasisEnd")
+	<-sub.Events()
+
+	if _, err := bridge.Play(bridge.ID(), "sound:confbridge-leave"); err != nil {
+		log.Error("failed to play leave sound", "error", err)
+	}
+
+	log.Debug("destroying bridge")
+	if err := bridge.Delete(); err != nil {
+		log.Error("failed to delete bridge", "error", err)
+	}
+	delete(bridges, bridge.ID())
+}
+
+func JoinBridge(cl ari.Client, bridgeID string, ext string) error {
+	bridge := bridges[bridgeID]
+	if bridge == nil {
+		return errors.New("this bridge does not exist")
+	}
+
+	if err := originate(cl, bridge, ext); err != nil {
+		return err
+	}
+
+	log.Debug("joining bridge successful")
+	return nil
+}
+
+func addEndpoints(cl ari.Client, bridge *ari.BridgeHandle, exts []string) error {
+	for _, ext := range exts {
+		if err := originate(cl, bridge, ext); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func originate(cl ari.Client, bridge *ari.BridgeHandle, ch string) error {
+	channel, err := createChannel(cl, ch)
+	if err != nil {
+		log.Error("failed to create channel", "error", err)
+		return err
+	}
+
+	if err := channel.Dial("ARI", 10*time.Second); err != nil {
+		log.Error("dial failed", "error", err)
+		return err
 	}
 
 	if err := bridge.AddChannel(channel.ID()); err != nil {
 		log.Error("add channel failed", "error", err)
+		return err
 	}
+
+	return nil
 }
 
-func createChannel(cl ari.Client, endpoint string) *ari.ChannelHandle {
-	//Create new channel
+func createChannel(cl ari.Client, ext string) (*ari.ChannelHandle, error) {
 	channel, err := cl.Channel().Create(&ari.Key{}, ari.ChannelCreateRequest{
-		Endpoint: "PJSIP/" + endpoint,
+		Endpoint: "PJSIP/" + ext,
 		App:      "ari-app",
 	})
 	if err != nil {
-		log.Error("failed to create channel", "error", err)
-		return nil
+		return nil, err
 	}
 
-	//Add to an existing map of channels and return to caller
-	channels[channel.ID()] = "PJSIP" + endpoint
-	return channel
+	channels[channel.ID()] = "PJSIP/" + ext
+	return channel, nil
 }
 
 func createBridge(cl ari.Client) *ari.BridgeHandle {
-	//Create a new bridge handle
 	bridge, err := cl.Bridge().Create(&ari.Key{}, "mixing", "")
-
 	if err != nil {
 		log.Error("failed to create bridge", "error", err)
 		return nil
 	}
 
-	//Add to an existing map of bridges and return to caller
 	bridges[bridge.ID()] = bridge
 	return bridge
 }
 
-func readInput() (string, []string) {
+func readInput() (action string, args []string) {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
+	//Split input into an array of strings
 	arr := strings.Fields(strings.Replace(text, "\n", "", -1))
 	return arr[0], arr[1:]
-	// return "Dial", []string{"5000", "5001"} //Testing
 }
